6947fbba295053628b938b6e7a7bc6d891db1eec
49hacker <paranshian+@gmail.com>
2012-06-22 13:28:31 +0900

    changed Router

--- core/Router.php
+++ core/Router.php
@@ -4,43 +4,51 @@ class Router {
 	protected $routes;
 
 	public function __construct($definitions) {
-		$this -> routes = $this -> compileRoutes($definitions);
+		$this->routes = $this->compileRoutes($definitions);
 	}
 
+	// ルーティング定義配列を変換する
+	// array( '/user/:id' => array('controller' => 'user', 'action' => 'show');
+	// みたいなやつ
 	public function compileRoutes($definitions) {
 		$routes = array();
 
 		foreach ($$definitions as $url => $params) {
+			// スラッシュ(/)でURLを分割する
 			$tokens = explode('/', ltrim($url, '/'));
 			foreach ($tokens as $i => $token) {
+				// コロンで始まる文字列の場合は正規表現の形式に変更する
 				if (0 === strpos($token, ':')) {
 					$name = substr($token, 1);
+					// 名前付きキャプチャ
 					$token = '(?P<' . $name . '>[^/]+)';
 				}
 				$tokens[$i] = $token;
 			}
-
+			// 分割した（変換済み）URLを再度スラッシュでつなげる
 			$pattern = '/' . implode('/', $tokens);
 			$routes[$pattern] = $params;
 		}
-		
+
 		return $routes;
 	}
-	
+
+	// PATH_INFOを受け取りマッチングを行う
 	public function resolve($path_info){
+		// 先頭がスラッシュでない場合、先頭にスラッシュを付与する
 		if('/' !== substr($path_info, 0, 1)){
 			$path_info = '/' . $path_info;
 		}
-		
-		foreach ($$this->routes as $pattern => $params) {
+
+		foreach ($this->routes as $pattern => $params) {
 			if(preg_match('#^' . $pattern . '$#', $path_info, $matches)){
 				$params = array_merge($params, $matches);
-				
+
 				return $params;
 			}
 		}
-		
+
 		return false;		
 	}
-	
+
 }
